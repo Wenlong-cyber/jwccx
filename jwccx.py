d6bb42aba92410ac82142dce1a17cf3332ca2c06
Wenlong-cyber <63028219+Wenlong-cyber@users.noreply.github.com>
2022-05-08 19:21:13 +0800

    Update jwccx.py

--- jwccx.py
+++ jwccx.py
@@ -224,7 +224,7 @@ if __name__ == "__main__":
             st.session_state['login'] = '1'
             st.text("登录成功！")
     # 选择功能
-    func_option = st.selectbox("选择你想要的功能", ('成绩查询', '考试安排', '评教'))
+    func_option = st.selectbox("选择你想要的功能", ('请选择','成绩查询', '考试安排', '评教'))
     if st.session_state['login']!="":
         if func_option=="成绩查询":
             st.dataframe(st.session_state['data'][['KCM','ZCJ']])
